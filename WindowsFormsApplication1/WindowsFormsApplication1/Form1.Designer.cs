fdb2e2f9deb44bd6f7990c93c9546fbf49e1f99e
Roma Sen <roma1off1ac@gmail.com>
2020-01-18 21:27:52 +0300

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(611, 16);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the number of restrictions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter the number of free members";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 85);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 55;
+            this.dataGridView1.Size = new System.Drawing.Size(625, 514);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.DataGridView1_EditingControlShowing);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(700, 20);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 55;
+            this.dataGridView2.Size = new System.Drawing.Size(625, 514);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(695, 554);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x*";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(695, 599);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "y*";
+            this.label4.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 612);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(625, 81);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Solve the problem using the Simplex method";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 52);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(528, 26);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "START!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MAX",
+            "MIN"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 52);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(695, 635);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 29);
+            this.label5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 700);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "SimplexMethod";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//Performed By Sen. R. S.
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool check = true;
+        bool can_we = true;
+        static int col = 0, row = 0;
+        int[] razresh = new int[2];
+        int iter = 1;
+        //Initialize form3
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+        //Creating a table for entering initial values
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Refresh();
+            col = Convert.ToInt32(textBox2.Text); row = Convert.ToInt32(textBox1.Text);
+            dataGridView1.RowCount = row + 1;
+            dataGridView1.ColumnCount = (col + row) + 1;
+            dataGridView1.Visible = true;
+            button2.Visible = true;
+            //Basic variable
+            dataGridView1.TopLeftHeaderCell.Value = "B.V.";
+            //Key column
+            dataGridView1.Columns[0].HeaderText = "B.K.";
+            for (int i = 1; i < dataGridView1.ColumnCount; i++)
+            {
+                dataGridView1.Columns[i].HeaderText = "X" + string.Format((i).ToString(), "0");
+            }
+            for (int i = 0; (i < (dataGridView1.Rows.Count - 1)); i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = "X" + string.Format((i + col + 1).ToString(), "0");
+            }
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].HeaderCell.Value = "F";
+        }
+
+        //Validation
+        private void TextBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) || (e.KeyChar == Convert.ToChar(",")) || (e.KeyChar == '\b')) 
+                return;
+            else
+            {
+                char c = e.KeyChar;
+                e.Handled = c >= 'а' && c <= 'я' || c >= 'А' && c <= 'Я' || c >= 'ё' && c <= 'Ё' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+            }
+        }
+
+        //Validation
+        private void TextBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) || (e.KeyChar == Convert.ToChar(",")) || (e.KeyChar == '\b')) 
+                return;
+            else
+            {
+                char c = e.KeyChar;
+                e.Handled = c >= 'а' && c <= 'я' || c >= 'А' && c <= 'Я' || c >= 'ё' && c <= 'Ё' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+            }
+        }
+
+        //Validation for table
+        private void DataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = (TextBox)e.Control;
+            tb.KeyPress += new KeyPressEventHandler(Tb_KeyPress);
+        }
+
+        //Validation
+        void Tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                { e.Handled = true; }
+            }
+        }
+        //simplex method
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //When building a new solution, we update the old table
+            dataGridView2.Refresh();
+            dataGridView2.Update();
+            for (int i = 0; i <= dataGridView2.RowCount - 1; i++)
+                for (int j = 0; j <= dataGridView2.ColumnCount - 1; j++)
+                    dataGridView2[j, i].Style.BackColor = System.Drawing.Color.White;
+            double[,] mass = new double[dataGridView1.RowCount, dataGridView1.ColumnCount];
+            dataGridView2.RowCount = dataGridView1.RowCount;
+            dataGridView2.ColumnCount = dataGridView1.ColumnCount;
+            dataGridView2.TopLeftHeaderCell.Value = "B.V.";
+            dataGridView2.Columns[0].HeaderText = "B.K.";
+            for (int i = 1; i < dataGridView2.ColumnCount; i++)
+            {
+                dataGridView2.Columns[i].HeaderText = "X" + string.Format((i).ToString(), "0");
+            }
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].HeaderCell.Value = "F";
+            dataGridView2.Rows[dataGridView1.Rows.Count - 1].HeaderCell.Value = "F";
+            for (int i = 0; i <= dataGridView1.RowCount - 1; i++)
+                for (int j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    if (dataGridView1[j, i].Value != DBNull.Value)
+                        mass[i, j] = Convert.ToDouble(dataGridView1[j, i].Value);
+                    else
+                        mass[i, j] = 0;
+                }
+            double[,] temp_mass = new double[dataGridView1.RowCount, dataGridView1.ColumnCount];
+            for (int i = 0; i <= dataGridView1.RowCount - 1; i++)
+                for (int j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                    temp_mass[i, j] = mass[i, j];
+            // MAX or MIN
+            if (comboBox1.SelectedIndex == 1)
+            {
+                check = true;
+                can_we = true;
+                iter = 1;
+                MessageBox.Show("MIN", "START!!!");
+                while (check)
+                {
+                    print(temp_mass);
+                    razresh = MINFindingResolvingElem(temp_mass, dataGridView1.ColumnCount - 1, dataGridView1.RowCount - 1);
+                    //CHECKING FOR OPTIMALITY
+                    if (!check)
+                    {
+                        for (int i = 0; (i <= (dataGridView2.ColumnCount - 1)); i++)
+                        {
+                            dataGridView2[i, dataGridView2.RowCount - 1].Style.BackColor = System.Drawing.Color.LightGreen;
+                        }
+                        break;
+                    }
+                    //Checking if there is a solution
+                    if (!can_we)
+                    {
+                        MessageBox.Show("This system has no solution!", "Sorry, we have error!");
+                        dataGridView2.Refresh();
+                        label3.Text = "";
+                        label4.Text = "";
+                        break;
+                    }
+                    temp_mass = find_next(temp_mass, razresh, col, row); 
+                    dataGridView2[razresh[1], razresh[0] + dataGridView2.RowCount - dataGridView1.RowCount].Style.BackColor = System.Drawing.Color.Purple;
+                }
+                print_result(); //CHECK for the NUMBER of SOLUTIONS(infinity or not)
+                label5.Text = $"MIN = " + (double)dataGridView2[0, dataGridView2.RowCount - 1].Value;
+            }
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                check = true;
+                can_we = true;
+                iter = 1;
+                MessageBox.Show("MAX", "START!!!");
+                while (check)
+                {
+                    print(temp_mass);
+                    razresh = MAXFindingResolvingElem(temp_mass, dataGridView1.ColumnCount - 1, dataGridView1.RowCount - 1);
+                    if (!check)
+                    {
+                        for (int i = 0; (i <= (dataGridView2.ColumnCount - 1)); i++)
+                        {
+                            dataGridView2[i, dataGridView2.RowCount - 1].Style.BackColor = System.Drawing.Color.LightGreen;
+                        }
+                        break;
+                    }
+                    if (!can_we)
+                    {
+                        MessageBox.Show("This system has no solution!", "Sorry, we have error!");
+                        dataGridView2.Refresh();
+                        label3.Text = "";
+                        label4.Text = "";
+                        break;
+                    }
+                    temp_mass = find_next(temp_mass, razresh, col, row);
+                    dataGridView2[razresh[1], razresh[0] + dataGridView2.RowCount - dataGridView1.RowCount].Style.BackColor = System.Drawing.Color.Purple;
+                }
+                print_result(); 
+                label5.Text = $"MAX = " + (double)dataGridView2[0, dataGridView2.RowCount - 1].Value;
+            }
+            dataGridView2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+        }
+
+        //Looking for a resolving element
+        public int[] MAXFindingResolvingElem(double[,] massiv, int column, int row)
+        {
+            bool temp = false;
+            int[] koord = new int[2];
+            double save_inf = double.MaxValue;
+            for (int i = 0; i < column; i++)
+            {
+                if (massiv[row, i] < 0)
+                {
+                    if (massiv[row, i] < save_inf)
+                    {
+                        //if the past is negative < new then save the coordinates
+                        save_inf = massiv[row, i]; 
+                        koord[1] = i;
+                    }
+                    //as long as we have at least one negative
+                    temp = true;    
+                }
+            }
+            //if no negative element is found, then this is the optimal plan
+            check = temp; 
+            temp = true;
+            save_inf = double.MaxValue;
+            double temp1 = double.MaxValue;
+            for (int i = 0; i < row; i++)
+            {
+                //if we can calculate the coefficient and select the resolution element
+                if (massiv[i, 0] > 0 && massiv[i, koord[1]] > 0) 
+                {
+                    temp1 = (massiv[i, 0] / massiv[i, koord[1]]);
+                    //if the new ratio is less than the previous one, save the rows and update the value
+                    if (temp1 < save_inf) 
+                    {
+                        save_inf = temp1;
+                        koord[0] = i;
+                    }
+                    temp = false;
+                }
+            }
+            //otherwise there is no solution
+            if (temp)
+            {
+                can_we = false;
+            }
+            return koord;
+
+        }
+        //To search for the highest-resolution element when solving a minimum problem
+        public int[] MINFindingResolvingElem(double[,] massiv, int column, int row)
+        {
+            bool temp = false;
+            int[] koord = new int[2];
+            double save_inf = double.MinValue;
+            for (int i = 0; i < column; i++) 
+            {
+                if (massiv[row, i] > 0) 
+                {
+                    if (massiv[row, i] > save_inf)
+                    {
+                        save_inf = massiv[row, i]; 
+                        koord[1] = i;
+                    }
+                    temp = true; 
+                }
+            }
+            check = temp;
+            temp = true;
+            save_inf = double.MaxValue;
+            double temp1 = double.MaxValue;
+            for (int i = 0; i < row; i++)
+            {
+                if (massiv[i, 0] > 0 && massiv[i, koord[1]] > 0) 
+                {
+                    temp1 = (massiv[i, 0] / massiv[i, koord[1]]);
+                    if (temp1 < save_inf) 
+                    {
+                        save_inf = temp1;
+                        koord[0] = i;
+                    }
+                    temp = false;
+                }
+            }
+            if (temp)
+            {
+                can_we = false;
+            }
+            return koord;
+        }
+        //Looking for the following table
+        public double[,] find_next(double[,] massiv, int[] razresh, int column, int row)
+        {
+            double[,] last_vers = new double[dataGridView1.RowCount, dataGridView1.ColumnCount];
+            for (int i = 0; i <= dataGridView1.RowCount - 1; i++)
+                for (int j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    if (j == razresh[1]) 
+                        last_vers[i, j] = 0;
+                    else if (i == razresh[0]) 
+                        last_vers[i, j] = massiv[i, j] / massiv[razresh[0], razresh[1]];
+                    else
+                        last_vers[i, j] = ((massiv[i, j] * massiv[razresh[0], razresh[1]]) - (massiv[razresh[0], j] * massiv[i, razresh[1]])) / massiv[razresh[0], razresh[1]];
+                }
+            last_vers[razresh[0], razresh[1]] = 1;
+            return last_vers;
+        }
+        //Displaying an array on the screen
+        public void print(double[,] massiv)
+        {
+            int K = 0;
+            dataGridView2.RowCount = dataGridView1.RowCount * iter;
+            for (int i = dataGridView2.RowCount - dataGridView1.RowCount; i <= dataGridView2.RowCount - 1; i++) 
+            {
+                for (int j = 0; j <= (col + row); j++)
+                {
+                    dataGridView2[j, i].Value = Math.Round(massiv[K, j], 3);
+                }
+                K++;
+            }
+            if (iter == 1)
+            {
+                for (int i = dataGridView2.RowCount - dataGridView1.RowCount; (i < (dataGridView2.Rows.Count - 1)); i++)
+                {
+                    dataGridView2.Rows[i].HeaderCell.Value = "X" + string.Format((i + col + 1).ToString(), "0");
+                }
+                dataGridView2.Rows[dataGridView2.RowCount - 1].HeaderCell.Value = "F";
+                for (int i = 0; (i <= dataGridView2.ColumnCount - 1); i++) 
+                {
+                    dataGridView2[i, dataGridView2.RowCount - 1].Style.BackColor = System.Drawing.Color.Red;
+                }
+            }
+            else if (iter >= 2)
+            {
+                for (int i = dataGridView2.RowCount - dataGridView1.RowCount; (i < (dataGridView2.Rows.Count - 1)); i++)
+                {
+                    dataGridView2.Rows[i].HeaderCell.Value = dataGridView2.Rows[i - dataGridView1.RowCount].HeaderCell.Value;
+                }
+                dataGridView2.Rows[dataGridView2.RowCount - 1].HeaderCell.Value = "F";
+                for (int i = 0; (i <= (dataGridView2.ColumnCount - 1)); i++)
+                {
+                    dataGridView2[i, dataGridView2.RowCount - 1].Style.BackColor = System.Drawing.Color.Red;
+                }
+                dataGridView2.Rows[razresh[0] + dataGridView1.RowCount * iter - dataGridView1.RowCount].HeaderCell.Value = "X" + string.Format((razresh[1]).ToString(), "0");
+            }
+            iter++;
+        }
+        // Output X*and Y*
+        public void print_result()
+        {
+            bool besckone4 = false;
+            string x = "", y = "";
+            for (int i = col + 1; i < dataGridView2.ColumnCount; i++)
+            {
+                y += ($" {(double)dataGridView2[i, dataGridView2.RowCount - 1].Value}; ");
+
+            }
+            for (int i = 1; i < dataGridView2.ColumnCount; i++)
+            {
+                x += ($" {Find_x(i)}; ");
+                if (i <= col)
+                {
+                    y += ($" {(double)dataGridView2[i, dataGridView2.RowCount - 1].Value}; ");
+                }
+                else if (Find_x(i) == 0)
+                    besckone4 = true;
+            }
+            label3.Text = "X* (" + x + ")";
+            label4.Text = "Y* (" + y + ")";
+            if (besckone4)
+                MessageBox.Show("This system has an infinite set of solutions [in place of the free member is 0]!", "I think I should warn you :)");
+        }
+
+      
+        //Assisting method for searching for X
+        public double Find_x(int x)
+        {
+            double next_x = 0;
+            for (int i = dataGridView2.RowCount - dataGridView1.RowCount; (i < (dataGridView2.RowCount - 1)); i++)
+            {
+                if (((string)dataGridView2.Rows[i].HeaderCell.Value).Equals("X" + string.Format((x).ToString(), "0")))
+                    next_x = (double)dataGridView2[0, i].Value;
+            }
+            return next_x;
+        }
+    }
+}
